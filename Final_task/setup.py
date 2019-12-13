121446590e0f3ae2e1faf5503aed0eed5b3e99fc
JustgoodDeal <stostoraz@gmail.com>
2019-12-13 22:54:47 +0300

    Made RSS_reader. Version: Iteration 5

---
+++ Final_task/setup.py
@@ -0,0 +1,17 @@
+import os
+import rss_reader
+import setuptools
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='rss-reader',
+    version=rss_reader.__version__,
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.7',
+    install_requires=['feedparser>=5.1.3', 'beautifulsoup4>=4.4.1',
+                      'colored==1.4.2', 'EbookLib==0.17.1']
+)
